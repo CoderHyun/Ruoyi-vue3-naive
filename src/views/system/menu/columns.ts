5927112ba194972d82c1c1d2caa6f8abdfb7aca0
Dengxian <dengxian@gentlehealth.cn>
2022-06-06 17:40:31 +0800

    All file submit

---
+++ src/views/system/menu/columns.ts
@@ -0,0 +1,10 @@
+import { ColumnsType } from '/#/index';
+
+export const columns: ColumnsType[] = [
+  { key: 'menuName', title: '菜单名称' },
+  { key: 'icon', title: '图标' },
+  { key: 'perms', title: '权限标识' },
+  { key: 'component', title: '组件路径' },
+  { key: 'status', title: '状态' },
+  { key: 'createTime', title: '创建时间' },
+];
--- src/views/system/menu/menu.vue
+++ src/views/system/menu/menu.vue
@@ -1,131 +1,33 @@
 <template>
   <div>
     <CreateDrawer ref="createDrawerRef" :title="drawerTitle" />
+    <BasicTable :columns="columns" :data-source="treeData" :pagination="false" />
   </div>
 </template>
 <script lang="ts" setup>
-  import { ref, unref, reactive, onMounted, computed } from 'vue';
-  import { useMessage } from 'naive-ui';
-  import { DownOutlined, AlignLeftOutlined, SearchOutlined, FormOutlined } from '@vicons/antd';
+  import { ref, onMounted } from 'vue';
   import { getMenuList } from '@/api/system/menu';
-  import { awaitWrap, getTreeItem, handleTree } from '@/utils';
+  import { awaitWrap, handleTree } from '@/utils';
   import CreateDrawer from './CreateDrawer.vue';
   import { MenuType } from '@/types/system/menuType';
+  import { BasicTable } from '@/components/Table';
+  import { columns } from './columns';
 
-  const rules = {
-    label: {
-      required: true,
-      message: '请输入标题',
-      trigger: 'blur',
-    },
-    path: {
-      required: true,
-      message: '请输入路径',
-      trigger: 'blur',
-    },
-  };
-
-  const formRef: any = ref(null);
   const createDrawerRef = ref();
-  const message = useMessage();
-
-  let treeItemKey = ref([]);
-
-  let expandedKeys = ref([]);
-
   const treeData = ref([]);
-
   const loading = ref(true);
-  const subLoading = ref(false);
-  const isEditMenu = ref(false);
-  const treeItemTitle = ref('');
-  const pattern = ref('');
   const drawerTitle = ref('');
 
-  const isAddSon = computed(() => {
-    return !treeItemKey.value.length;
-  });
-
-  const addMenuOptions = ref([
-    {
-      label: '添加顶级菜单',
-      key: 'home',
-      disabled: false,
-    },
-    {
-      label: '添加子菜单',
-      key: 'son',
-      disabled: isAddSon,
-    },
-  ]);
-
-  const formParams = reactive({
-    type: 1,
-    label: '',
-    subtitle: '',
-    path: '',
-    auth: '',
-    openType: 1,
-  });
-
-  function selectAddMenu(key: string) {
-    drawerTitle.value = key === 'home' ? '添加顶栏菜单' : `添加子菜单：${treeItemTitle.value}`;
-    openCreateDrawer();
-  }
-
-  function openCreateDrawer() {
-    const { openDrawer } = createDrawerRef.value;
-    openDrawer();
-  }
-
-  function selectedTree(keys) {
-    if (keys.length) {
-      const treeItem = getTreeItem(unref(treeData), keys[0]);
-      treeItemKey.value = keys;
-      treeItemTitle.value = treeItem.label;
-      Object.assign(formParams, treeItem);
-      isEditMenu.value = true;
-    } else {
-      isEditMenu.value = false;
-      treeItemKey.value = [];
-      treeItemTitle.value = '';
-    }
-  }
-
-  function handleReset() {
-    const treeItem = getTreeItem(unref(treeData), treeItemKey.value[0]);
-    Object.assign(formParams, treeItem);
-  }
-
-  function formSubmit() {
-    formRef.value.validate((errors: boolean) => {
-      if (!errors) {
-        message.error('抱歉，您没有该权限');
-      } else {
-        message.error('请填写完整信息');
-      }
-    });
-  }
-
-  function packHandle() {
-    if (expandedKeys.value.length) {
-      expandedKeys.value = [];
-    } else {
-      expandedKeys.value = unref(treeData).map((item: any) => item.key as string) as [];
-    }
-  }
-
-  onMounted(async () => {
+  const getMenuTreeData = async () => {
     const [data, err] = await awaitWrap<MenuType>(getMenuList());
     if (err) {
       loading.value = false;
       return false;
     }
     treeData.value = handleTree(data, 'menuId');
-    loading.value = false;
-  });
+  };
 
-  function onExpandedKeys(keys) {
-    expandedKeys.value = keys;
-  }
+  onMounted(async () => {
+    await getMenuTreeData();
+  });
 </script>
